78db51c289e516b5a55390e8e5bbd511fe52b3f3
Yang Wu <yang@Yangs-Laptop.local>
2025-04-02 19:44:36 -0500

    add script and frontend

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express();
 //   next();
 // };
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 // app.use(requestLogger);
 morgan.token('req-body', function (req, res) {
